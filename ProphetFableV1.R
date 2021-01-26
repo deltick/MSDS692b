c61c544549beb714934d26486b5d7829672c0dfe
JJT <jjtamer@gmail.com>
2021-01-26 15:26:14 -0500

    s

--- ProphetFableV1.R
+++ ProphetFableV1.R
@@ -7,8 +7,51 @@ library(fable)
 library(dplyr)
 library(reshape)
 
+
+
+urlfile="https://raw.githubusercontent.com/deltick/MSDS692b/main/Data/USBPTS.csv"
+
+inputTS<-read_csv(url(urlfile))
+
+inputSel <- inputTS %>% select(xDate, 'San Diego', 'El Paso', 'Laredo', 'Rio Grande Valley', 'Del Rio', 'El Centro', 'Tucson', 'Yuma')
+
+inputSel <- dplyr::rename(inputSel, SDC = 'San Diego')
+inputSel <- dplyr::rename(inputSel, ELP = 'El Paso')
+inputSel <- dplyr::rename(inputSel, LRT = 'Laredo')
+inputSel <- dplyr::rename(inputSel, RGV = 'Rio Grande Valley')
+inputSel <- dplyr::rename(inputSel, DRT = 'Del Rio')
+inputSel <- dplyr::rename(inputSel, ELC = 'El Centro')
+inputSel <- dplyr::rename(inputSel, TUS = 'Tucson')
+inputSel <- dplyr::rename(inputSel, YUM = 'Yuma')
+
+#inputSel <- select(inputSel, -xDate)
+
+inputMelt <- melt(as.data.frame(inputSel), id="xDate") #, variable_name = "count")
+
+#inputTs2 <- ts(inputSel, start=c(2000, 1), frequency=12)
+
+inputTsb <- as_tsibble(inputMelt, index=xDate, key=variable)
+
+#inputTsb %>% autoplot(vars(SD, RGV, LRT, ELP))
+
+
 inputTsb %>% autoplot(value)
 
+
+inputSel2 <- inputTS %>% select(xDate, "Southwest Border")
+inputSel2 <- dplyr::rename(inputSel2, SWB = "Southwest Border")
+inputMelt2 <- melt(as.data.frame(inputSel2), id="xDate")
+
+inputMelt2 <- inputMelt2 %>%
+  mutate(Month = yearmonth(xDate)) %>%
+  as_tsibble(index = Month)
+
+inputMelt2 <- select(inputMelt2, -xDate)
+
+inputTsb2 <- as_tsibble(inputMelt2, index=Month, key=variable)
+inputTsb2 %>% autoplot()
+
+########################################################################
 
 
 fit <- inputTsb %>% model(
@@ -53,7 +96,7 @@ fc2 %>% accuracy(inputTsb)
 library(lubridate)
 
 
-recent_production <- inputTsb %>%
+recent_production <- inputTsb2 %>%
   filter(year(xDate) >= 2010)
 recent_production %>%
   autoplot(value) 
--- TsEDAv1.R
+++ TsEDAv1.R
@@ -91,5 +91,72 @@ swbTib %>%
   time_recompose() %>%
   plot_anomalies(time_recomposed=TRUE, ncol=3, alpha_dots=0.25)
 
+swbTib %>%
+  time_decompose(SWB) %>%
+  anomalize(remainder) %>%
+  plot_anomaly_decomposition() +
+  labs(title = "Decomposition of Anomalized Lubridate Downloads")
 
 
+library(tidyverse)
+library(tidyquant)
+library(anomalize)
+library(timetk)
+swbTib %>%
+  ggplot(aes(xDate, SWB)) +
+  geom_point(alpha = 0.5) +
+  #facet_wrap(~ package, ncol = 3, scales = "free_y") +
+  scale_color_viridis_d() +
+  theme_tq() 
+
+swb_anomalized <- swbTib %>%
+  time_decompose(SWB) %>%
+  anomalize(remainder) %>%
+  
+  # Function to clean & repair anomalous data
+  clean_anomalies()
+
+swb_anomalized %>%
+  plot_time_series(xDate, swb_anomalized$observed_cleaned)
+
+swb_anomalized %>%
+  plot_time_series(xDate, swb_anomalized$observed)
+
+
+swb_anomalized %>%
+  forecast_mae(col_train = observed, col_test = observed, prop = 0.8)
+
+swb_anomalized %>%
+  forecast_mae(col_train = observed_cleaned, col_test = observed, prop = 0.8)
+
+######################################################################
+forecast_mae <- function(data, col_train, col_test, prop = 0.8) {
+  
+  predict_expr <- enquo(col_train)
+  actual_expr <- enquo(col_test)
+  
+  idx_train <- 1:(floor(prop * nrow(data)))
+  
+  train_tbl <- data %>% filter(row_number() %in% idx_train)
+  test_tbl  <- data %>% filter(!row_number() %in% idx_train)
+  
+  # Model using training data (training) 
+  model_formula <- as.formula(paste0(quo_name(predict_expr), " ~ index.num + year + quarter + month.lbl + day + wday.lbl"))
+  
+  model_glm <- train_tbl %>%
+    tk_augment_timeseries_signature() %>%
+    glm(model_formula, data = .)
+  
+  # Make Prediction
+  suppressWarnings({
+    # Suppress rank-deficit warning
+    prediction <- predict(model_glm, newdata = test_tbl %>% tk_augment_timeseries_signature()) 
+    actual     <- test_tbl %>% pull(!! actual_expr)
+  })
+  
+  # Calculate MAE
+  mae <- mean(abs(prediction - actual))
+  
+  return(mae)
+  
+}
